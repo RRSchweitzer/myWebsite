9ff230a6c9a700abcd185c6b0e17b49e2db7dbff
Ryan <RR.Schweitzer@gmail.com>
2016-10-23 21:29:29 -0400

    removed a bunch of themes and added my own custom theme

---
+++ wp-content/themes/practice_theme/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ryan1
+ * Date: 10/23/16
+ * Time: 9:24 PM
+ */
---
+++ wp-content/themes/ryan/Header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ryan1
+ * Date: 10/23/16
+ * Time: 8:39 PM
+ */
